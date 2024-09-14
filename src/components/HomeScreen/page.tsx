d6663004bb8cacc98beaf6b4fac133bb966ce39a
Richard Mezzomo <richardmezzomo1@gmail.com>
2024-09-14 17:57:01 -0300

    style: font-size email

--- src/components/HomeScreen/page.tsx
+++ src/components/HomeScreen/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function HomeScreen() {
   return (
     <>
-      <section className="relative -z-10 flex h-screen w-full justify-between bg-[#F8F7F3] pt-12 text-black">
+      <section className="-z-10 flex h-screen w-full justify-between bg-[#F8F7F3] pt-12 text-black">
         <main className="h-auto px-6 pt-14 lg:flex lg:flex-col lg:justify-center lg:pl-20 lg:pt-0">
           <p className="text-gray-500 mb-2 font-mono text-sm lg:mb-4 lg:text-lg">
             olá, meu nome é
@@ -23,23 +23,25 @@ export default function HomeScreen() {
           </p>
 
           <div className="flex w-full items-center justify-center pt-14 lg:flex lg:justify-start lg:pt-16">
-            <Link href="https://wa.me/+5548984864299" target="_blank">
-              <div className="border border-black px-6 py-2 font-mono text-sm transition-all duration-300 hover:bg-black hover:text-white lg:px-12 lg:py-3 lg:text-lg">
+            <Link href="https://wa.me/+5548984864299" passHref legacyBehavior>
+              <a
+                target="_blank"
+                className="border border-black px-6 py-2 font-mono text-sm transition-all duration-300 hover:bg-black hover:text-white lg:px-12 lg:py-3 lg:text-lg"
+              >
                 ENTRE EM CONTATO
-              </div>
+              </a>
             </Link>
           </div>
         </main>
 
-        {/* Div rotacionada e fixada no lado inferior direito */}
-        <button className="absolute -bottom-[75px] right-[-120px] mb-56 flex rotate-90 items-center">
-          <Link href="mailto:richardbbmm@gmail.com">
-            <div className="text-gray-600 font-mono text-sm">
+        <div className="-bottom-[75px] right-[-120px] mb-56 flex rotate-90 items-center lg:-bottom-[52px]">
+          <Link href="mailto:richardbbmm@gmail.com" passHref legacyBehavior>
+            <a className="text-gray-600 font-mono text-sm lg:text-lg">
               richardbbmm@gmail.com
-            </div>
+            </a>
           </Link>
           <div className="ml-4 h-0.5 w-32 bg-black"></div>
-        </button>
+        </div>
       </section>
     </>
   )
