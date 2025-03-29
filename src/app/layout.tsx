5e44aae244018c118d99c76e9b1e9e03b10b80d9
richardbmezzomo <richardmezzomo1@gmail.com>
2025-03-29 19:45:19 -0300

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,38 +1,21 @@
-import type { Metadata } from 'next'
-// eslint-disable-next-line camelcase
-import { Montserrat, Source_Code_Pro } from 'next/font/google'
-import './globals.css'
-
-const montserrat = Montserrat({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400', '500', '600', '700'],
-  variable: '--font-montserrat',
-})
-
-const sourceCodePro = Source_Code_Pro({
-  subsets: ['latin'],
-  display: 'swap',
-  weight: ['400', '500', '700'],
-  variable: '--font-source-code-pro',
-})
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Richard Mezzomo - Desenvolvedor Web',
-  description: 'Desenvolvedor Web',
-}
+  title: "Richard B Mezzomo",
+  description: "Portfolio",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html
-      lang="en"
-      className={`${montserrat.variable} ${sourceCodePro.variable}`}
-    >
-      <body>{children}</body>
+    <html lang="pt-br">
+      <body>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,5 @@
-import About from '@/components/About/page'
-import Contact from '@/components/Contact/page'
-import HomeScreen from '@/components/HomeScreen/page'
-import Navbar from '@/components/Navbar/page'
-import Portfolio from '@/components/Portfolio/page'
-
 export default function Home() {
   return (
-    <>
-      <Navbar />
-      <HomeScreen />
-      <About />
-      <Portfolio />
-      <Contact />
-    </>
-  )
+    <h1>Hello world!</h1>
+  );
 }
